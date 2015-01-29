b903e6f8d61d3c1666adcb6cd6cf129d4775d591
pimg <pim@rubix.nl>
2015-01-29 10:39:20 +0100

    changed processors to beans to remove some bloat from the mapping classes

--- orderprocessing/src/main/java/nl/rubix/codefest/orderprocessing/mappings/SqlToSelectOrderIdsResponse.java
+++ orderprocessing/src/main/java/nl/rubix/codefest/orderprocessing/mappings/SqlToSelectOrderIdsResponse.java
@@ -1,6 +1,5 @@
 package nl.rubix.codefest.orderprocessing.mappings;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -9,25 +8,14 @@ import nl.rubix.orderprocessing.orderinfoservice.GetOrderIdsByDistrictResponse.O
 import nl.rubix.orderprocessing.orderinfoservice.GetOrderIdsByDistrictResponse.Orders.Order;
 import nl.rubix.orderprocessing.orderinfoservice.ObjectFactory;
 
-import org.apache.camel.Exchange;
-import org.apache.camel.Message;
-import org.apache.camel.Processor;
+public class SqlToSelectOrderIdsResponse {
 
-public class SqlToSelectOrderIdsResponse implements Processor{
-
-	@Override
-	public void process(Exchange exchange) throws Exception {
-		// retrieve the in message from the Camel exchange
-		Message in = exchange.getIn();
-		
-		//retrieve the object from the message, this contains the response of the query
-		//The results will be mapped to a List (one entry for each row) of Maps (one entry for each column using the column name as the key). 
-		//Each element in the list will be of the form returned by this interface's queryForMap() methods.
-		List<Map<String, Object>> request = in.getBody(ArrayList.class);
+	public GetOrderIdsByDistrictResponse SqlToGetOrderByDistrictMapping(List<Map<String, Object>> request){
 		//initialize the response object
 		ObjectFactory objFac = new ObjectFactory();
 		GetOrderIdsByDistrictResponse response = objFac.createGetOrderIdsByDistrictResponse();
 		Orders orders = objFac.createGetOrderIdsByDistrictResponseOrders();
+		//the List object of the request contains an entry for each row returned by the SQL statement. We need to loop through every row and create an order object
 		for (Map<String, Object> col : request) {
 			String id = (String) col.get("id");
 			Order order = objFac.createGetOrderIdsByDistrictResponseOrdersOrder();
@@ -35,8 +23,8 @@ public class SqlToSelectOrderIdsResponse implements Processor{
 			orders.getOrder().add(order);
 		}
 		response.setOrders(orders);
-		in.setBody(response);
-	
+		
+		return response;
 	}
 
 }
